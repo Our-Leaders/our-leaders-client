02a67da864dfd8d356a85e6184988d57a59b1f36
saint-lawal <abiso.lawal@gmail.com>
2019-11-21 00:53:03 +0100

    - Add general styling

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,68 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+body, html, #app {
+  font-family: 'PT Serif', serif;
+  height: 100%;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+.field {
+  border-bottom: 1px solid black;
+  outline: none;
+}
+
+.btn-primary,
+.btn-primary:focus,
+.btn-primary:active {
+  outline: none;
+  @apply bg-primary font-semibold p-3 text-white;
+
+  &.loading {
+    animation: loading 1.5s linear infinite;
+  }
+}
+
+.btn-primary:hover {
+  background: rgba(206, 152, 51, 0.9);
+}
+
+.input-fields {
+  border-bottom: 1px solid black;
+
+  input {
+    outline: none;
+  }
+}
+
+// This is just an idea for an animation, it can change
+@keyframes loading {
+  0% {
+    background: #CE9833;
+  }
+
+  25% {
+    background: #D6A853;
+  }
+
+  50% {
+    background: #DDB872;
+  }
+
+  75% {
+    background: #D6A853;
+  }
+
+  100% {
+    background: #CE9833;
+  }
 }
 </style>
