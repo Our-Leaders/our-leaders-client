371a9f33e9b38c57aa7a5adce163ec06570adf8e
saint-lawal <abiso.lawal@gmail.com>
2019-11-30 22:23:00 +0100

    - Add route for sign up and guard for already signed in users

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Pages from '../views/index';
+import Store from '../store/index';
 
 Vue.use(VueRouter);
 
@@ -15,12 +16,19 @@ const routes = [
     path: '/auth/sign-in',
     name: 'sign-in',
     component: Pages.SignIn,
-    meta: { layout: 'no-navbar' },
+    meta: {
+      isAuth: true,
+      layout: 'no-navbar',
+    },
   },
   {
     path: '/auth/sign-up',
     name: 'sign-up',
     component: Pages.SignUp,
+    meta: {
+      isAuth: true,
+      layout: 'no-navbar',
+    },
   },
 ];
 
@@ -28,6 +36,15 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.isAuth && Store.getters.isLoggedIn) {
+    // next(from.path);
+    next();
+  } else {
+    next();
+  }
 });
 
 export default router;
