1d47833dcc0110c3323bba5d6086dc4bbbea4e94
saint-lawal <abiso.lawal@gmail.com>
2019-12-07 01:07:29 +0100

    - Add parent div to accomodate slot

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,65 +1,68 @@
 <template>
-<nav class="flex items-center justify-between flex-wrap border-nav xl:border-b xl:m-0 px-6 py-4 xl:p-0 shadow-md xl:shadow-none">
-  <router-link :to="{ name: 'home' }" class="nav-icon"><img class="xl:absolute mr-2 w-12" src="@/assets/img/logo.svg"/></router-link>
-  <div class="block lg:hidden">
-    <button @click="toggleNav" :class="menuToggleClass" class="nav-menu flex items-center focus:outline-none">
-      <span></span>
-      <span></span>
-      <span></span>
-    </button>
-  </div>
-  <div :class="navToggleClass" class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-    <div class="text-base lg:flex-grow">
-      <router-link :to="{ name: '' }" class="block mt-4 lg:inline-block lg:mt-0 mr-4">Leaders</router-link>
-      <router-link :to="{ name: '' }" class="block mt-4 lg:inline-block lg:mt-0 mr-4">Political parties</router-link>
+<div>
+  <nav class="flex items-center justify-between flex-wrap border-nav xl:border-b xl:m-0 px-6 py-4 xl:p-0 shadow-md xl:shadow-none">
+    <router-link :to="{ name: 'home' }" class="nav-icon"><img class="xl:absolute w-12 p-2 bg-white z-10" src="@/assets/img/logo.svg"/></router-link>
+    <div class="block lg:hidden">
+      <button @click="toggleNav" :class="menuToggleClass" class="nav-menu flex items-center focus:outline-none">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
     </div>
-    <div>
-      <ul class="relative lg:inline-flex lg:items-center">
-        <li>
-          <router-link :to="{ name: '' }" class="block lg:px-4 lg:py-2 lg:border-b border-transparent hover:border-primary mt-4 lg:mt-0">About</router-link>
-        </li>
-        <li>
-          <router-link :to="{ name: '' }" class="block lg:px-4 lg:py-2 lg:border-b border-transparent hover:border-primary mt-4 lg:mt-0">Contact</router-link>
-        </li>
-        <li class="donate-wrapper">
-          <router-link :to="{ name: '' }" class="px-4 py-2 mt-4 lg:ml-4 lg:mt-0 btn-primary-outline block leading-tight">Donate</router-link>
-        </li>
-        <li class="xl:absolute lg:ml-4 h-16 lg:border-r lg:border-l-0 lg:border-t-0 lg:border-b-0 border right-0 w-full mt-4 lg:mt-0">
-          <div class="search-wrapper h-full relative mr-2 xl:mr-8">
-            <input v-model="searchQuery" type="text" ref="search" class="search-input h-full w-full xl:w-6 xl:opacity-0 pr-6 xl:pl-0 pl-4 py-2" placeholder="Search" />
-            <img class="search-img" src="@/assets/img/search.svg"/>
-            <img @click="clearSearch" class="search-clear" src="@/assets/img/close.svg"/>
-          </div>
-        </li>
-      </ul>
-      <ul class="lg:inline-flex lg:items-center">
-        <li class="lg:ml-9 lg:mr-2" v-if="!isLoggedIn">
-          <router-link :to="{ name: 'sign-up' }" class="mt-4 px-4 py-2 lg:mt-0 btn-primary block leading-tight">Sign up</router-link>
-        </li>
-        <li v-if="!isLoggedIn">
-          <router-link :to="{ name: 'sign-in' }" class="mt-4 px-4 py-2 lg:mt-0 border border-primary lg:border-b lg:border-transparent hover:border-primary w-full block leading-tight font-circular">Log in</router-link>
-        </li>
-        <li class="lg:ml-9 lg:mr-2" v-if="isLoggedIn">
-          <div class="flex justify-center h-8 w-8 p-2 relative rounded-full bg-gray-300 text-xs">
-            <span class="absolute top-0 right-0 bg-red-600 h-3 w-3 rounded-full"></span>
-            <span class="inline-block align-middle">12</span>
-          </div>
-        </li>
-        <li class="lg:ml-2" v-if="isLoggedIn">
-          <div class="flex justify-center h-8 w-8 overflow-hidden rounded-full bg-gray-100 text-xs">
-            <img class="object-cover object-center" src="../assets/img/user.svg"/>
-          </div>
-        </li>
-        <li>
-          <a href="#" class="flex lg:py-2 mt-4 lg:ml-9 lg:mt-0 border lg:border-0 py-3 px-2 justify-between">
-            <img class="pr-4" src="@/assets/img/flags/nigeria.svg"/>
-            <img src="@/assets/img/chevron-down.svg"/>
-          </a>
-        </li>
-      </ul>
+    <div :class="navToggleClass" class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="text-base lg:flex-grow">
+        <router-link :to="{ name: '' }" class="block mt-4 lg:inline-block lg:mt-0 mr-4">Leaders</router-link>
+        <router-link :to="{ name: '' }" class="block mt-4 lg:inline-block lg:mt-0 mr-4">Political parties</router-link>
+      </div>
+      <div>
+        <ul class="relative lg:inline-flex lg:items-center">
+          <li>
+            <router-link :to="{ name: '' }" class="block lg:px-4 lg:py-2 lg:border-b border-transparent hover:border-primary mt-4 lg:mt-0">About</router-link>
+          </li>
+          <li>
+            <router-link :to="{ name: '' }" class="block lg:px-4 lg:py-2 lg:border-b border-transparent hover:border-primary mt-4 lg:mt-0">Contact</router-link>
+          </li>
+          <li class="donate-wrapper">
+            <router-link :to="{ name: '' }" class="px-4 py-2 mt-4 lg:ml-4 lg:mt-0 btn-primary-outline block leading-tight">Donate</router-link>
+          </li>
+          <li class="xl:absolute lg:ml-4 h-16 lg:border-r lg:border-l-0 lg:border-t-0 lg:border-b-0 border right-0 w-full mt-4 lg:mt-0">
+            <div class="search-wrapper h-full relative mr-2 xl:mr-8">
+              <input v-model="searchQuery" type="text" ref="search" class="search-input h-full w-full xl:w-6 xl:opacity-0 pr-6 xl:pl-0 pl-4 py-2" placeholder="Search" />
+              <img class="search-img" src="@/assets/img/search.svg"/>
+              <img @click="clearSearch" class="search-clear" src="@/assets/img/close.svg"/>
+            </div>
+          </li>
+        </ul>
+        <ul class="lg:inline-flex lg:items-center">
+          <li class="lg:ml-9 lg:mr-2" v-if="!isLoggedIn">
+            <router-link :to="{ name: 'sign-up' }" class="mt-4 px-4 py-2 lg:mt-0 btn-primary block leading-tight">Sign up</router-link>
+          </li>
+          <li v-if="!isLoggedIn">
+            <router-link :to="{ name: 'sign-in' }" class="mt-4 px-4 py-2 lg:mt-0 border border-primary lg:border-b lg:border-transparent hover:border-primary w-full block leading-tight font-circular">Log in</router-link>
+          </li>
+          <li class="lg:ml-9 lg:mr-2" v-if="isLoggedIn">
+            <div class="flex justify-center h-8 w-8 p-2 relative rounded-full bg-gray-300 text-xs cursor-pointer">
+              <span class="absolute top-0 right-0 bg-red-600 h-3 w-3 rounded-full"></span>
+              <span class="inline-block align-middle">12</span>
+            </div>
+          </li>
+          <li class="lg:ml-2" v-if="isLoggedIn">
+            <div class="flex justify-center h-8 w-8 overflow-hidden rounded-full bg-gray-100 text-xs cursor-pointer">
+              <img class="object-cover object-center" src="../assets/img/user.svg"/>
+            </div>
+          </li>
+          <li>
+            <a href="#" class="flex lg:py-2 mt-4 lg:ml-6 lg:mt-0 border lg:border-0 py-3 px-2 justify-between">
+              <img class="pr-4" src="@/assets/img/flags/nigeria.svg"/>
+              <img src="@/assets/img/chevron-down.svg"/>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
+  <slot/>
+</div>
 </template>
 
 <script>
