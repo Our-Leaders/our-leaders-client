d9c8bc01926d092175bdc7ebb091842727923e9e
saint-lawal <abiso.lawal@gmail.com>
2019-12-07 01:08:45 +0100

    - Add politician page

--- src/views/index.js
+++ src/views/index.js
@@ -1,9 +1,11 @@
 import Home from './public/Home.vue';
+import Politicians from './public/Politicians.vue';
 import SignIn from './auth/SignIn.vue';
 import SignUp from './auth/SignUp.vue';
 
 export default {
   Home,
+  Politicians,
   SignIn,
   SignUp,
 };
