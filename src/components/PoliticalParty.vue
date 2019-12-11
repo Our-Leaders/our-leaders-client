f723fbc8bcd8caff904f447ae66908959d60f90b
saint-lawal <abiso.lawal@gmail.com>
2019-12-11 19:03:54 +0100

    - Add political party component

--- src/components/PoliticalParty.vue
+++ src/components/PoliticalParty.vue
@@ -8,31 +8,24 @@
     </div>
   </div>
   <div class="w-full overflow-hidden h-20 md:h-32 lg:h-40">
-    <img :src="politician.profileImage" class="w-full object-cover"/>
+    <img :src="party.logo" class="w-full object-cover"/>
   </div>
   <div class="w-full my-1 md:my-2">
     <img class="inline-block mr-1 md:mr-2 h-3 md:h-4" src="../assets/img/thumbs-up.svg"/>
-    <span class="inline-block mr-2 md:mr-4 h-3 md:h-4 align-middle text-xs md:text-sm">{{politician.vote.up}}</span>
+    <span class="inline-block mr-2 md:mr-4 h-3 md:h-4 align-middle text-xs md:text-sm">{{party.vote.up}}</span>
     <img class="inline-block mr-1 md:mr-2 h-3 md:h-4" src="../assets/img/thumbs-down.svg"/>
-    <span class="inline-block mr-1 md:mr-2 h-3 md:h-4 align-middle text-xs md:text-sm">{{politician.vote.down}}</span>
+    <span class="inline-block mr-1 md:mr-2 h-3 md:h-4 align-middle text-xs md:text-sm">{{party.vote.down}}</span>
   </div>
-  <h4 class="font-semibold text-base md:text-lg">{{politician.name}}</h4>
-  <h4 class="capitalize overflow-ellipse text-sm md:text-base">{{position}}</h4>
+  <h4 class="font-semibold text-base md:text-lg">{{party.name}}</h4>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'politician',
+  name: 'political-party',
   props: {
-    politician: {
+    party: {
       type: Object,
-    },
-  },
-  computed: {
-    position() {
-      const background = this.politician.politicalBackground.find(x => x.inOffice);
-      return `${background.position} of ${background.institution}`;
     },
   },
 };
