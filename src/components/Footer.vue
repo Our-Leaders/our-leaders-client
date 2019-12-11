d311d06b1ff3afdf98721ff1ec9bd665451822a7
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-11 16:08:45 +0100

    ft: add the footer into the default rendering pipeline

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,9 +3,9 @@
 </template>
 
 <script>
-  export default {
-    name: "Footer"
-  }
+export default {
+  name: 'our-footer',
+};
 </script>
 
 <style scoped>
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 
 import Alert from '@/components/Alert.vue';
 import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
 
 Vue.component('our-alert', Alert);
 Vue.component('our-navbar', Navbar);
+Vue.component('our-footer', Footer);
