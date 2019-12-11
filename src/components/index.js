b201592c97f402e2d21e209577763f6cb5f8b207
saint-lawal <abiso.lawal@gmail.com>
2019-12-11 19:04:52 +0100

    - Add political parties route

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Vue from 'vue';
 import Alert from '@/components/Alert.vue';
 import Navbar from '@/components/Navbar.vue';
 import Pagination from '@/components/Pagination.vue';
+import PoliticalParty from '@/components/PoliticalParty.vue';
 import Politician from '@/components/Politician.vue';
 import Tabs from '@/components/Tabs.vue';
 
 Vue.component('our-alert', Alert);
 Vue.component('our-navbar', Navbar);
 Vue.component('our-pagination', Pagination);
+Vue.component('our-political-party', PoliticalParty);
 Vue.component('our-politician', Politician);
 Vue.component('our-tabs', Tabs);
