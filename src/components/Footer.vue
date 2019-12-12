f466f595e853b0fec13022b5cab786e498d1b024
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-12 14:36:54 +0100

    ft: update html

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,37 @@
 <template>
-<div>
-  I am footer
-</div>
+  <div>
+    <div>
+      <div>
+
+      </div>
+      <div>
+        <h3>Company</h3>
+        <a href="#">About us</a>
+        <a href="#">Contact us</a>
+        <a href="#">Careers</a>
+        <a href="#">Donate</a>
+      </div>
+      <div>
+        <h3>Company</h3>
+        <a href="#">Contact us</a>
+        <a href="#">FAQs</a>
+      </div>
+      <div>
+        <h3>Links</h3>
+        <a href="#">Privacy Policy</a>
+        <a href="#">Sitemap</a>
+      </div>
+      <div>
+        <h3>Socials</h3>
+        <div>
+
+        </div>
+      </div>
+    </div>
+    <div>
+      &copy; Olubrain Leadership Foundation, 2019
+    </div>
+  </div>
 </template>
 
 <script>
