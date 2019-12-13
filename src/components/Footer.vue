36e599ed54431628570403f5d6345af80fbe5b41
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-13 15:43:47 +0100

    ft: update styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,22 @@
 <template>
-  <div>
-    <div>
-      <div>
-        <div>
-          <img class="xl:absolute mr-2 w-12" src="@/assets/img/logo.svg"/>
+  <div class="footer">
+    <div class="footer-elements">
+      <div class="footer-col">
+        <div class="footer-logo-elements">
+          <img class="footer-logo" src="@/assets/img/logo.svg"/>
         </div>
-        <div>
-
+        <div class="footer-social-elements">
+          <a href="#"><img class="social-icon" src="@/assets/img/facebook-black.svg"/></a>
+          <a href="#"><img class="social-icon" src="@/assets/img/twitter-black.svg"/></a>
+          <a href="#"><img class="social-icon" src="@/assets/img/linkedin-black.svg"/></a>
+          <a href="#"><img class="social-icon" src="@/assets/img/instagram-black.svg"/></a>
         </div>
       </div>
       <div class="footer-col">
         <h3>Contact</h3>
         <a href="#">8, Lawal Street, off Oweh street, Jibowu, Yaba Lagos, Nigeria</a>
-        <a href="#">(234) 817 543 9754</a>
-        <a href="#">Careers</a>
+        <a href="tel:+2348175439754">(234) 817 543 9754</a>
+        <a href="mailto:hello@ourloaders.africa">hello@ourleaders.africa</a>
       </div>
       <div class="footer-col">
         <h3>Company</h3>
@@ -42,20 +45,60 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.footer-col {
-  font-family: 'Circular Std',serif;
+  .footer {
+    font-family: 'Circular Std', serif;
+    width: 100%;
 
-  h3 {
-    color: #929497;
-    font-size: 16px;
-    margin-bottom: 6px;
+    .footer-elements {
+      width: 87%;
+      margin-left: auto;
+      margin-right: auto;
+      border-style: solid;
+      border-color: #dbdbdb;
+      border-top-width: 1px;
+      padding: 50px 40px;
+      display: flex;
+      flex-direction: row;
+
+      .footer-col {
+        width: 20%;
+
+        h3 {
+          color: #929497;
+          font-size: 16px;
+          margin-bottom: 6px;
+        }
+
+        a {
+          font-size: 14px;
+          text-decoration: none;
+          margin-top: 10px;
+          display: block;
+        }
+
+        .footer-logo-elements {
+          display: flex;
+          justify-content: center;
+        }
+
+        .footer-social-elements {
+          display: flex;
+          margin-top: 20px;
+          justify-content: center;
+        }
+      }
+    }
   }
 
-  a {
-    font-size: 14px;
-    text-decoration: none;
-    display: block;
-    margin-top: 10px;
+  .footer-logo {
+    width: 100px;
+    height: 100px;
   }
-}
+
+  .social-icon {
+    width: 22px;
+    height: 22px;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
 </style>
