3bd264fc7b72200b80118d608438b7d884bbfde2
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-13 13:07:40 +0100

    ft: add initial formatting for the headers

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,34 +2,35 @@
   <div>
     <div>
       <div>
-
-      </div>
-      <div>
-        <h3>Company</h3>
-        <a href="#">About us</a>
-        <a href="#">Contact us</a>
-        <a href="#">Careers</a>
-        <a href="#">Donate</a>
-      </div>
-      <div>
-        <h3>Company</h3>
-        <a href="#">Contact us</a>
-        <a href="#">FAQs</a>
-      </div>
-      <div>
-        <h3>Links</h3>
-        <a href="#">Privacy Policy</a>
-        <a href="#">Sitemap</a>
-      </div>
-      <div>
-        <h3>Socials</h3>
+        <div>
+          <img class="xl:absolute mr-2 w-12" src="@/assets/img/logo.svg"/>
+        </div>
         <div>
 
         </div>
       </div>
-    </div>
-    <div>
-      &copy; Olubrain Leadership Foundation, 2019
+      <div class="footer-col">
+        <h3>Contact</h3>
+        <a href="#">8, Lawal Street, off Oweh street, Jibowu, Yaba Lagos, Nigeria</a>
+        <a href="#">(234) 817 543 9754</a>
+        <a href="#">Careers</a>
+      </div>
+      <div class="footer-col">
+        <h3>Company</h3>
+        <a href="#">About us</a>
+        <a href="#">Blog</a>
+        <a href="#">Careers</a>
+      </div>
+      <div class="footer-col">
+        <h3>Help</h3>
+        <a href="#">Contact us</a>
+        <a href="#">FAQs</a>
+      </div>
+      <div class="footer-col">
+        <h3>Links</h3>
+        <a href="#">Privacy Policy</a>
+        <a href="#">Sitemap</a>
+      </div>
     </div>
   </div>
 </template>
@@ -40,6 +41,21 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.footer-col {
+  font-family: 'Circular Std',serif;
 
+  h3 {
+    color: #929497;
+    font-size: 16px;
+    margin-bottom: 6px;
+  }
+
+  a {
+    font-size: 14px;
+    text-decoration: none;
+    display: block;
+    margin-top: 10px;
+  }
+}
 </style>
