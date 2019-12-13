5d7709479c3d867cf718c3018ce67edbabfc4ccc
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-13 17:25:57 +0100

    ft: add white svgs

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -65,7 +65,7 @@
          <div class="footer-col">
            <h3>Socials</h3>
            <div class="footer-social-elements">
-             <a href="#"><img class="social-icon" src="@/assets/img/facebook-black.svg"/></a>
+             <a href="#"><img class="social-icon" src="@/assets/img/facebook_gray.svg"/></a>
              <a href="#"><img class="social-icon" src="@/assets/img/twitter-black.svg"/></a>
              <a href="#"><img class="social-icon" src="@/assets/img/linkedin-black.svg"/></a>
              <a href="#"><img class="social-icon" src="@/assets/img/instagram-black.svg"/></a>
