917f5c95bf84e34a12edeb2988f6b7c4c44d7d60
saint-lawal <abiso.lawal@gmail.com>
2019-12-21 23:50:20 +0100

    - Add labels component

---
+++ src/components/Labels.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="flex flex-wrap">
+    <span class="inline-block label border-2 border-nav mr-2 mb-2 px-2 py-1"
+      v-for="(option, i) of options"
+      :class="{'active': isLabel(option.value)}"
+      @click="setLabel(option.value)"
+      :key="i">
+      {{option.label}}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'labels',
+  props: {
+    options: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      label: null,
+    };
+  },
+  methods: {
+    isLabel(label) {
+      return this.label === label;
+    },
+    setLabel(label) {
+      this.label = label;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+  .label {
+    cursor: pointer;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+
+    &:hover,
+    &.active {
+      border: 2px solid #CE9833;
+      color: #CE9833;
+    }
+  }
+</style>
