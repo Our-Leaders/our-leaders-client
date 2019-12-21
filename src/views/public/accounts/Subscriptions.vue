003b22dc99af61d0dc8882e4a24c2213f132e2d7
saint-lawal <abiso.lawal@gmail.com>
2019-12-21 23:51:31 +0100

    - Add subscriptions page

---
+++ src/views/public/accounts/Subscriptions.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="pr-10 lg:pr-64 xl:pr-64">
+    <h3 class="text-4xl mb-8">Manage Subscriptions</h3>
+    <our-labels :options="labels"></our-labels>
+    <div class="w-full" v-for="(politician, i) in politicians" :key="i">
+      <our-politician :politician="politician" view-type="secondary"></our-politician>
+    </div>
+  </div>
+</template>
+
+<script>
+import labels from '../../../assets/json/politicalPositions.json';
+import { politiciansMock } from '../../../constants/examples';
+
+export default {
+  name: 'manage-subscriptions',
+  data() {
+    return {
+      labels: labels.politicalPositions,
+      politicians: politiciansMock,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
