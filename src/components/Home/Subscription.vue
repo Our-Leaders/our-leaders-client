a548fcddd43a002fd7998e6a3df729d9126d058a
chinezenwosu <chineze.nwosu@andela.com>
2019-12-28 21:24:06 +0100

    create subscription component with minimal styling

---
+++ src/components/Home/Subscription.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="subscription block relative">
+  <p class="subscribe-text mb-4"><span class="text-primary">Subscribe</span> to our newsletter</p>
+  <small class="text-xs">Be the first to get updates on your favorite leaders.</small>
+  <div class="relative w-full h-20 md:h-32 lg:h-40">
+    <input type="text" v-model="email"/>
+    <button type="submit" @click="subscribe" :disabled="!email">Subscribe</button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'subscription',
+  data() {
+    return {
+      email: '',
+    };
+  },
+  methods: {
+    subscribe() {
+      console.log('subscribed: ', this.email);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .subscribe-text {
+    font-size: 3.75rem;
+  }
+</style>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 
 import TrendingPolitician from '@/components/Home/TrendingPolitician.vue';
+import Subscription from '@/components/Home/Subscription.vue';
 
 Vue.component('our-trending-politician', TrendingPolitician);
+Vue.component('our-subscription', Subscription);
