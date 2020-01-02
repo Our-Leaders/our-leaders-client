e39bdec57d83a75ae5098c7075354eb7c9353a00
chinezenwosu <chineze.nwosu@andela.com>
2020-01-02 22:32:53 +0100

    change TrendingPolitician to TrendingPoliticians

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 
-import TrendingPolitician from '@/components/Home/TrendingPolitician.vue';
+import TrendingPoliticians from '@/components/Home/TrendingPoliticians.vue';
 import Subscription from '@/components/Home/Subscription.vue';
 import Politicians from '@/components/Home/Politicians.vue';
 
-Vue.component('our-trending-politician', TrendingPolitician);
+Vue.component('our-trending-politicians', TrendingPoliticians);
 Vue.component('our-subscription', Subscription);
 Vue.component('our-home-politicians', Politicians);
