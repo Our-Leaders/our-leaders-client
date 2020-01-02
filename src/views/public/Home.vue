a9934b0b1c5e7c9bcc3f746cff37d004667cbeef
chinezenwosu <chineze.nwosu@andela.com>
2020-01-02 12:01:22 +0100

    fix tab mobile version in home

--- src/views/public/Home.vue
+++ src/views/public/Home.vue
@@ -28,17 +28,9 @@
                   <span class="loading lg mx-auto"></span>
                 </div>
               </div>
-              <a @click="toggle(-1)" class="toggle-arrow toggle-arrow-left">
-                <img src="@/assets/img/chevron-left.svg"/>
-              </a>
-              <div class="flex flex-wrap mb-4 min-h-64" v-if="!loading">
-                <div class="politician-container w-1/2 md:w-2/6 my-6" v-for="(politician, i) in politicians" :key="i">
-                  <our-politician :politician="politician" :total="politicians.length"></our-politician>
-                </div>
+              <div v-if="!loading">
+                <our-home-politicians :politicians="politicians" ></our-home-politicians>
               </div>
-              <a @click="toggle(1)" class="toggle-arrow toggle-arrow-right">
-                <img src="@/assets/img/chevron-right.svg"/>
-              </a>
             </div>
           </div>
         </div>
@@ -47,31 +39,36 @@
       <div class="flex mb-40">
         <div class="relative w-full mx-auto h-auto">
           <div class="bg-gray-fa px-12 py-32">
-            <div class="flex justify-start what-to-do-section">
+            <div class="flex justify-start flex-col lg:flex-row what-to-do-section">
               <div class="what-to-do-left-section">
                 <h1>What <span class="text-primary">you</span> <span class="whitespace-pre">can do here</span></h1>
                 <p class="mb-10 mt-6">Get instant updates on your favourite African leaders.</p>
-                <span class="mt-4 px-4 py-2 lg:mt-0 btn-primary inline-block w-40 text-center">View leaders</span>
+                <router-link :to="{ name: 'politicians' }" class="px-4 py-2 mt-0 btn-primary inline-block w-40 text-center hidden lg:block">View leaders</router-link>
               </div>
               <div class="what-to-do-right-section w-full xl:ml-16 relative">
-                <div class="what-to-do-right-content what-to-do-subscribe flex justify-between items-center bg-white absolute">
-                  <p class="what-to-do-subscribe-text text-2xl py-6">Subscribe to your favourite leaders</p>
-                  <div class="relative subscribe-hand">
-                    <span class="absolute"><img src="@/assets/img/subscribe-hand.svg"/></span>
+                <div class="what-to-do-right-container">
+                  <div class="what-to-do-right-content what-to-do-subscribe flex justify-between items-center bg-white relative lg:absolute">
+                    <p class="what-to-do-subscribe-text text-2xl py-6">Subscribe to your favourite leaders</p>
+                    <div class="relative subscribe-hand">
+                      <span class="absolute"><img src="@/assets/img/subscribe-hand.svg"/></span>
+                    </div>
+                  </div>
+                  <div class="what-to-do-right-content what-to-do-vote flex justify-between items-center bg-white relative lg:absolute">
+                    <p class="what-to-do-vote-text text-2xl py-6">Upvote or downvote leaders on the platform</p>
+                    <div class="relative vote-thumb">
+                      <span class="absolute"><img src="@/assets/img/thumbs-up-with-shards.svg"/></span>
+                    </div>
+                  </div>
+                  <div class="what-to-do-right-content what-to-do-view flex justify-between items-center bg-white relative lg:absolute">
+                    <p class="what-to-do-view-text text-2xl py-6">See up-to-date accomplishments of leaders</p>
+                    <div class="relative view-note">
+                      <span class="absolute"><img src="@/assets/img/note.svg"/></span>
+                    </div>
                   </div>
                 </div>
-                <div class="what-to-do-right-content what-to-do-vote flex justify-between items-center bg-white absolute">
-                  <p class="what-to-do-vote-text text-2xl py-6">Upvote or downvote leaders on the platform</p>
-                  <div class="relative vote-thumb">
-                    <span class="absolute"><img src="@/assets/img/thumbs-up-with-shards.svg"/></span>
-                  </div>
-                </div>
-                <div class="what-to-do-right-content what-to-do-view flex justify-between items-center bg-white absolute">
-                  <p class="what-to-do-view-text text-2xl py-6">See up-to-date accomplishments of leaders</p>
-                  <div class="relative view-note">
-                    <span class="absolute"><img src="@/assets/img/note.svg"/></span>
-                  </div>
-                </div>
+              </div>
+              <div class="lg:hidden">
+                <router-link :to="{ name: 'politicians' }" class="mt-8 px-4 py-2 lg:mt-0 btn-primary inline-block w-40 text-center">View leaders</router-link>
               </div>
             </div>
           </div>
@@ -90,6 +87,24 @@
               </div>
               <div class="collage-image image-3">
                 <img class="absolute" src="@/assets/img/leaders/nnamdi-azikiwe.svg"/>
+              </div>
+            </div>
+            <div class="w-screen lg:w-full flex justify-center relative">
+              <div class="collage-shape left">
+                <div class="rect left-rect absolute"></div>
+                <div class="dots left-dots absolute">
+                  <div class="block w-40 h-40 mx-auto">
+                    <span v-for="n in 25" :key="n" class="dot"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="quote-section">
+                <div class="quote-content">
+                  <h1>Our Children may learn about heroes of the past. Our task is to make ourselves architects of the future</h1>
+                  <span class="mt-4 px-4 py-2 lg:mt-0">- Jomo Kenyatta -</span>
+                </div>
+              </div>
+              <div class="collage-shape right">
                 <div class="rect right-rect absolute"></div>
                 <div class="dots right-dots absolute">
                   <div class="block w-40 h-40 mx-auto">
@@ -99,21 +114,7 @@
               </div>
             </div>
             <div>
-              <div class="quote-section">
-                <div class="quote-content">
-                  <h1>Our Children may learn about heroes of the past. Our task is to make ourselves architects of the future</h1>
-                  <span class="mt-4 px-4 py-2 lg:mt-0">- Jomo Kenyatta -</span>
-                </div>
-              </div>
-            </div>
-            <div>
               <div class="collage-image image-4">
-                <div class="rect left-rect absolute"></div>
-                <div class="dots left-dots absolute">
-                  <div class="block w-40 h-40 mx-auto">
-                    <span v-for="n in 25" :key="n" class="dot"></span>
-                  </div>
-                </div>
                 <img class="absolute" src="@/assets/img/leaders/jomo-kenyatta.svg"/>
               </div>
               <div class="collage-image image-5">
@@ -149,6 +150,9 @@ export default {
     async getPoliticians() {
       this.politicians = politiciansMock;
     },
+    setSecondary() {
+      this.getPoliticians();
+    },
   },
   computed: {
     totalPoliticians() {
@@ -162,20 +166,16 @@ export default {
   .bg-overlay {
     background-image: url('../../assets/img/bg-pattern-home.svg');
     background-size: 135% 100%;
-    margin-top: 30px;
 
     @screen xl {
       margin-top: -100px;
     }
   }
 
-  .body {
-    padding: 1.75rem 10rem;
-  }
-
   .header {
     margin: auto;
-    width: 100%;
+    padding: 0 0 5rem 0;
+    width: calc(100% - 3rem);
 
     @screen xl {
       padding: 8rem 10rem;
@@ -212,15 +212,31 @@ export default {
       }
 
       .what-to-do-right-section {
+        .what-to-do-right-container {
+          padding-top: 2rem;
+
+          @screen lg {
+            padding-top: 0;
+            position: absolute;
+            right: 0;
+            top: -9rem;
+          }
+        }
+
         .what-to-do-right-content {
-          box-shadow: 0px 12px 52px rgba(0, 0, 0, 0.1);
+          @apply shadow-primary;
           padding: 1.8rem 2rem;
           width: 31.25rem;
           z-index: 1;
 
           &.what-to-do-subscribe {
-            right: 3.125rem;
-            top: -9rem;
+            left: 0;
+
+            @screen lg {
+              right: 3.125rem;
+              top: 0;
+              left: initial;
+            }
 
             .what-to-do-subscribe-text {
               width: 12rem;
@@ -235,10 +251,16 @@ export default {
           }
 
           &.what-to-do-vote {
-            box-shadow: 0px 12px 52px rgba(0, 0, 0, 0.1);
+            @apply shadow-primary float-right;
             padding: 1.8rem 2rem;
-            right: -5.5rem;
-            top: 4rem;
+            margin: 1.375rem 0;
+
+            @screen lg {
+              float: none;
+              margin-top: 0;
+              right: -5.5rem;
+              top: 13rem;
+            }
 
             .what-to-do-vote-text {
               width: 15.8125rem;
@@ -253,9 +275,12 @@ export default {
           }
 
           &.what-to-do-view {
-            top: 17rem;
-            right: 1.125rem;
             width: 33rem;
+
+            @screen lg {
+              right: 1.125rem;
+              top: 26rem;
+            }
 
             .what-to-do-view-text {
               width: 19rem;
@@ -308,19 +333,43 @@ export default {
   .quote-section {
     position: relative;
     display: inline-block;
-    z-index: 10;
+    z-index: 1;
 
     .quote-content {
-      background: rgba(255, 255, 255, 0.6);
-      backdrop-filter: blur(20px);
       padding: 2.9375rem 2rem;
       text-align: center;
-      width: 536px;
+      width: 33.5rem;
+
+      @screen lg {
+        background: rgba(255, 255, 255, 0.6);
+        backdrop-filter: blur(20px);
+      }
 
       h1 {
-        font-size: 2.25rem;
-        line-height: 2.9375rem;
-        margin-bottom: 1.5rem;
+        font-size: 1.5rem;
+        line-height: 1.9375rem;
+        margin-bottom: 2.25rem;
+        position: relative;
+
+        &::before {
+          background: theme('colors.primary');
+          content: '';
+          height: 0.133rem;
+          left: calc(50% - 3.5rem);
+          position: absolute;
+          top: -1.5rem;
+          width: 7rem;
+        }
+
+        @screen md {
+          font-size: 2rem;
+          line-height: 2.9375rem;
+          margin-bottom: 1.5rem;
+        }
+
+        @screen lg {
+          font-size: 2.25rem;
+        }
       }
     }
   }
@@ -329,52 +378,23 @@ export default {
     align-items: center;
     display: flex;
     flex-direction: column;
-    height: 45rem;
     justify-content: space-around;
 
+    @screen lg {
+      height: 45rem;
+    }
+
     .collage-image {
-      display: inline-block;
+      display: none;
       position: relative;
       min-width: 15rem;
 
+      @screen lg {
+        display: inline-block;
+      }
+
       @screen xl {
         min-width: 20rem;
-      }
-
-      .dots {
-        &.left-dots {
-          left: -12.5rem;
-          bottom: -7rem;
-        }
-
-        &.right-dots {
-          right: -10rem;
-          top: 13rem;
-        }
-
-        .dot {
-          @apply inline-block mx-3 my-2 bg-primary;
-          height: 0.375rem;
-          width: 0.375rem;
-        }
-      }
-
-      .rect {
-        &.left-rect {
-          @apply bg-primary;
-          height: 6.375rem;
-          left: -13.125rem;
-          top: -11.5rem;
-          width: 5rem;
-        }
-
-        &.right-rect {
-          @apply bg-primary;
-          height: 2.5rem;
-          right: -0.925rem;
-          top: 9rem;
-          width: 2.5rem;
-        }
       }
 
       &.image-1 img {
@@ -427,6 +447,89 @@ export default {
 
           img {
             left: -6.25rem;
+          }
+        }
+      }
+    }
+
+    .collage-shape {
+      @apply absolute top-0 inline-block w-full;
+      height: 20rem;
+      overflow: hidden;
+
+      &.left {
+        @apply left-0 bottom-0;
+      }
+
+      &.right {
+        @apply right-0 top-0;
+      }
+
+      @screen lg {
+        width: auto;
+        height: auto;
+        overflow: initial;
+      }
+
+      .dots {
+        display: none;
+
+        &.left-dots {
+          left: 8.8rem;
+          top: calc(100% + 5.5rem);
+
+          @screen lg {
+            display: inline-block;
+          }
+        }
+
+        &.right-dots {
+          display: inline-block;
+          right: -7rem;
+          top: 9.8rem;
+          z-index: 1;
+
+          @screen lg {
+            right: 2.2rem;
+            top: 5.5rem;
+          }
+        }
+
+        .dot {
+          @apply inline-block mx-3 my-2 bg-primary;
+          height: 0.375rem;
+          width: 0.375rem;
+        }
+      }
+
+      .rect {
+        display: none;
+
+        &.left-rect {
+          @apply bg-primary;
+          height: 6.375rem;
+          left: 8.325rem;
+          bottom: -3.2rem;
+          width: 5rem;
+
+          @screen lg {
+            display: inline-block;
+          }
+        }
+
+        &.right-rect {
+          @apply bg-primary;
+          display: inline-block;
+          height: 2.5rem;
+          left: -1.25rem;
+          top: 0;
+          width: 2.5rem;
+          z-index: 1;
+
+          @screen lg {
+            right: 11.5rem;
+            top: 1rem;
+            left: initial;
           }
         }
       }
