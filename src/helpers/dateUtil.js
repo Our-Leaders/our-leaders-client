5e5d99fa635c39d25b6f0f2dd113301397837773
saint-lawal <abiso.lawal@gmail.com>
2020-01-05 23:47:01 +0100

    - Add date util

---
+++ src/helpers/dateUtil.js
@@ -0,0 +1,23 @@
+import ValidatorUtil from './validatorUtil';
+
+class DateUtil {
+  static getAge(dob) {
+    if (!ValidatorUtil.isDefined(dob)) { return 0; }
+
+    const dobDate = new Date(dob);
+    const today = new Date();
+    let age = today.getFullYear() - dobDate.getFullYear();
+
+    if (today.getMonth <= dobDate.getMonth && today.getDay() < dobDate.getDay()) {
+      age -= 1;
+    }
+
+    return age;
+  }
+
+  static getPeriodString(startDate, endDate) {
+    return `${(new Date(startDate)).getFullYear()} - ${(new Date(endDate)).getFullYear()}`;
+  }
+}
+
+export default DateUtil;
