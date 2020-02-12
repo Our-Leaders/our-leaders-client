96fb79195ee18c8559b3cfa888cd27d0d2d57699
saint-lawal <abiso.lawal@gmail.com>
2020-02-12 02:27:32 +0100

    - Update subscription component

--- src/components/Home/Subscription.vue
+++ src/components/Home/Subscription.vue
@@ -1,25 +1,70 @@
 <template>
-<div class="subscription block relative">
-  <p class="subscribe-text mb-4"><span class="text-primary">Subscribe</span> to our newsletter</p>
-  <p class="font-circular hidden md:block">Be the first to get updates on your favorite leaders.</p>
-  <form class="relative w-full subscribe-form px-6">
-    <input class="subscribe-input placeholder-gray-96 md:placeholder-transparent font-circular" placeholder="Enter email" type="text" v-model="email"/>
-    <button class="bg-primary border border-primary py-2 px-12 outline-none text-white h-full w-full md:w-auto" type="submit" @click="subscribe" :disabled="!email">Subscribe</button>
-  </form>
+<div class="w-full">
+  <div class="subscription block relative" v-if="!full">
+    <slot/>
+    <form class="relative w-full subscribe-form px-6">
+      <input class="subscribe-input placeholder-gray-96 md:placeholder-transparent font-circular" placeholder="Enter email" type="text" v-model="email" :disabled="processing"/>
+      <button class="bg-primary border border-primary py-2 px-12 outline-none text-white h-full w-full md:w-auto" type="submit" @click.prevent="subscribe" :disabled="!email || processing">Subscribe</button>
+    </form>
+  </div>
+  <div v-if="full">
+    <slot/>
+    <form>
+      <input class="field w-full mt-1 mb-6 py-2 pl-2 bg-white"
+        :disabled="processing"
+        type="text"
+        id="query"
+        name="query"
+        placeholder="Enter email"
+        v-model="email"/>
+      <button
+        class="btn-primary py-2 lg:py-0 xl:py-0 w-full h-full"
+        @click.prevent="subscribe"
+        :disabled="processing">
+        Subscribe
+      </button>
+    </form>
+  </div>
 </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   name: 'subscription',
+  props: {
+    full: {
+      type: Boolean,
+      default: false,
+    },
+  },
   data() {
     return {
-      email: '',
+      email: null,
+      processing: false,
+      subscriptionsServices: this.$serviceFactory.subscriptions,
     };
   },
   methods: {
-    subscribe() {
-      console.log('subscribed: ', this.email);
+    ...mapActions([
+      'displayError',
+      'displaySuccess',
+    ]),
+    async subscribe() {
+      try {
+        this.processing = true;
+        await this.subscriptionsServices.addSubscription({
+          email: this.email,
+          type: 'newsletter',
+        });
+        this.email = null;
+        this.processing = false;
+        this.displaySuccess({ message: 'You have successfully been subscribed to our news letter.' });
+      } catch (error) {
+        this.processing = false;
+        this.displayError(error);
+      }
     },
   },
 };
