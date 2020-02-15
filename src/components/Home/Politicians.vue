10f817751902d7ed470d1c7f313500df7ce554fe
saint-lawal <abiso.lawal@gmail.com>
2020-02-15 14:10:06 +0100

    - Home Page fix

--- src/components/Home/Politicians.vue
+++ src/components/Home/Politicians.vue
@@ -47,6 +47,10 @@ export default {
   },
   methods: {
     position(politician) {
+      if (politician.politicalBackground.length === 0) {
+        return 'Not in Office';
+      }
+
       const background = politician.politicalBackground.find(x => x.inOffice);
       return `${background.position}, ${background.institution}`;
     },
